8ddcf3f62b76801e6f680c3a710d575b565b52a1
Yumi Takuma <yumi.takuma@medistech.io>
2025-10-04 05:56:34 +0800

    Backup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,20 @@
-import Header from "@/components/Header";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Wrapper } from "@/components/ui/wrapper";
-import { cn } from "@/lib/utils";
-import dynamic from "next/dynamic";
-import { Lexend } from "next/font/google";
-import { ReactNode } from "react";
+import Header from '@/components/Header';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Wrapper } from '@/components/ui/wrapper';
+import { cn } from '@/lib/utils';
+import dynamic from 'next/dynamic';
+import { Lexend } from 'next/font/google';
+import { ReactNode } from 'react';
 
-const Footer = dynamic(() => import("@/components/Footer"), {
+const Footer = dynamic(() => import('@/components/Footer'), {
   ssr: false,
   loading: () => <Skeleton className="h-80 md:h-60 lg:h-56" />,
 });
 
 const lexend = Lexend({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-lexend",
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-lexend',
 });
 
 interface LayoutProps {
@@ -23,7 +23,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <Wrapper className={cn(lexend.variable, "min-h-screen")}>
+    <Wrapper className={cn(lexend.variable, 'min-h-screen')}>
       <Header />
       <main className="flex w-full flex-col">{children}</main>
       <Footer />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import RootLayout from "@/components/layout/Root";
-import "@/styles/globals.css";
-import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import Link from "next/link";
+import { PlasmicRootProvider } from '@plasmicapp/react-web';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Link from 'next/link';
+import '@/styles/globals.css';
+import Layout from '@/components/Layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head} Link={Link}>
-      <RootLayout>
-        <Component {...pageProps} />{" "}
-      </RootLayout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </PlasmicRootProvider>
   );
 }
